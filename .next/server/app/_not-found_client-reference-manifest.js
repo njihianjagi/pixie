0b1df63adfc7b85db445e37d968626c94b54da68
timonjagi <timothynjihia@gmail.com>
2025-03-20 00:22:01 +0300

    Update git ignore

--- .next/server/app/_not-found_client-reference-manifest.js
+++
@@ -1,8 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/_not-found"] = {
-  moduleLoading: { prefix: "/_next/", crossOrigin: null },
-  ssrModuleMapping: {},
-  edgeSSRModuleMapping: {},
-  clientModules: {},
-  entryCSSFiles: { "C:\\Users\\Admin\\Code\\pixie\\src\\app\\not-found": [] },
-};
